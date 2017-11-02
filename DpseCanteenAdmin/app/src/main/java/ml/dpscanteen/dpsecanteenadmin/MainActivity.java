ecd7347d8658acaabd56283d98422952453854f3
Infant Derrick Gnanasusairaj <infant.derrick.gnanasusairaj@gmail.com>
2017-11-02 09:34:35 +0530

    Made the actual Android files

---
+++ DpseCanteenAdmin/app/src/main/java/ml/dpscanteen/dpsecanteenadmin/MainActivity.java
@@ -0,0 +1,13 @@
+package ml.dpscanteen.dpsecanteenadmin;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
